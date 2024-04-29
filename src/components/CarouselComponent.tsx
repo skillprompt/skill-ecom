3bc165e95f43c32c5a955f2fce1866f8c682f8af
Sujal-Gaha-Magar <sujalgaha.skillprompt@gmail.com>
2024-04-29 11:47:12 +0545

    added the category bar and carousel

---
+++ src/components/CarouselComponent.tsx
@@ -0,0 +1,11 @@
+import { Carousel } from "antd";
+
+export function CarouselComponent() {
+  return (
+    <Carousel autoplay>
+      <img src="Banner1.png" alt="..." />
+      <img src="Banner2.png" alt="..." />
+      <img src="Banner3.png" alt="..." />
+    </Carousel>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,29 @@
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",flowbite.content()],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {
-
-      colors:{
-        btnColor:"#39844F",
-        btnTxtColor:"#ffffff",
-        inputColor:"#F0F5FF",
-        inputTxt:"#aaaaaa",
-        catogriesBg:"#2E2E2E",
-        hoverinputTxt:"#6C90C7",
-        light_black:"#292626",
-
-
+      colors: {
+        btnColor: "#39844F",
+        btnTxtColor: "#ffffff",
+        inputColor: "#F0F5FF",
+        inputTxt: "#aaaaaa",
+        catogriesBg: "#2E2E2E",
+        hoverinputTxt: "#6C90C7",
+        light_black: "#292626",
         loginBtnColour: "#68C3D4",
         createNewAccountBtnColour: "#39844F",
         buttonColour: "#39844F",
         hoverButtonColour: "#41975b",
         closeModalButtonColour: "#2098d5",
         forgotPasswordColour: "#6C90C7",
-        customNotificationColour: "#2f2f2f"
+        customNotificationColour: "#2f2f2f",
+        categoryBarColour: "#2E2E2E",
       },
     },
-    plugins: [
-      flowbite.plugin(),
-    ],
+    plugins: [flowbite.plugin()],
   },
 };
