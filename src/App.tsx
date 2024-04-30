d9218e2f3c6e2266d29c074a7284566b02217861
Sujal-Gaha-Magar <sujalgaha.skillprompt@gmail.com>
2024-04-30 11:12:14 +0545

    resolved conflict

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Register from "./components/Register";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { LoginPage } from "./components/LoginPage";
-<<<<<<< HEAD
 import Adminpanel from "./components/Adminpanel/AdminPanel";
 import Dashboard from "./components/Adminpanel/Dashboard";
 import Categories from "./components/Adminpanel/Categories";
@@ -12,20 +11,15 @@ import Products from "./components/Adminpanel/Products";
 import AddProducts from "./components/Adminpanel/AddProduct";
 import Orders from "./components/Adminpanel/Orders";
 import Users from "./components/Adminpanel/Users";
-=======
 import { Navbar } from "./components/Navbar";
-import { Toaster } from "sonner";
-import { NewPasswordForm } from "./components/NewPasswordForm";
 import { CategoryBar } from "./components/CategoryBar";
+import { Toaster } from "sonner";
 import { CarouselComponent } from "./components/CarouselComponent";
->>>>>>> e54ac510815d9026d554c68b81b7ea7b5c6ad084
+import { NewPasswordForm } from "./components/NewPasswordForm";
 
 const router = createBrowserRouter([
   {
     path: "/",
-<<<<<<< HEAD
-    element: "",
-=======
     element: (
       <>
         <Navbar />
@@ -55,13 +49,11 @@ const router = createBrowserRouter([
   {
     path: "/login/reset/:resetToken",
     element: <NewPasswordForm />,
->>>>>>> e54ac510815d9026d554c68b81b7ea7b5c6ad084
   },
   {
     path: "/register",
     element: <Register />,
   },
-<<<<<<< HEAD
   {
     path: "login",
     element: <LoginPage />,
@@ -100,8 +92,6 @@ const router = createBrowserRouter([
       },
     ],
   },
-=======
->>>>>>> e54ac510815d9026d554c68b81b7ea7b5c6ad084
 ]);
 
 const queryClient = new QueryClient();
