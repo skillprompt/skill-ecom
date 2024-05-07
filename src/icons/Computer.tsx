650b5201b64250d2bc97669b7b4e7bfeb579b094
Sujal-Gaha-Magar <sujalgaha.skillprompt@gmail.com>
2024-05-07 15:14:31 +0545

    made improvement to previous code base

---
+++ src/icons/Computer.tsx
@@ -0,0 +1,19 @@
+export default function ComputerIcon() {
+  return (
+    <svg
+      width="20"
+      height="18"
+      viewBox="0 0 20 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 17H15M7 13V17M13 13V17M1.42857 10.7143H18.5714M2 1H18C18.5523 1 19 1.44772 19 2V12C19 12.5523 18.5523 13 18 13H2C1.44772 13 1 12.5523 1 12V2C1 1.44772 1.44772 1 2 1Z"
+        stroke="white"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/icons/category-card-icons/DarkSmartWatch.tsx
@@ -0,0 +1,67 @@
+export default function DarkSmartWatchIcon() {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_979_89)">
+        <path
+          d="M30 12H18C14.6863 12 12 14.6863 12 18V30C12 33.3137 14.6863 36 18 36H30C33.3137 36 36 33.3137 36 30V18C36 14.6863 33.3137 12 30 12Z"
+          stroke="black"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M18 36V42H30V36"
+          stroke="black"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M18 12V6H30V12"
+          stroke="black"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <line
+          x1="20.7148"
+          y1="18"
+          x2="20.7148"
+          y2="29"
+          stroke="black"
+          strokeWidth={2}
+          strokeLinecap="round"
+        />
+        <line
+          x1="24.7148"
+          y1="23"
+          x2="24.7148"
+          y2="29"
+          stroke="black"
+          strokeWidth={2}
+          strokeLinecap="round"
+        />
+        <line
+          x1="28.7148"
+          y1="21"
+          x2="28.7148"
+          y2="29"
+          stroke="black"
+          strokeWidth={2}
+          strokeLinecap="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_979_89">
+          <rect width="48" height="48" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
